3264c955aa4fc60c3ba797ba772dfdc5776960d0
gerrickb <27402037+GerrickB@users.noreply.github.com>
2022-12-05 21:03:47 -0800

    Add code to collect and display legislator names

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -28,9 +28,16 @@ contents.each do |row|
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     )
     legislators = legislators.officials
+
+    legislator_names = legislators.map do |legislator|
+      legislator.name
+    end
+
+    legislators_string = legislator_names.join(",")
+
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 
-  puts "#{name} #{zip_code} #{legislators}"
+  puts "#{name} #{zip_code} #{legislators_string}"
 end
