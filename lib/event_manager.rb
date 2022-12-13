1d35f8f52da40794b5b1605805d9dfd41789d272
gerrickb <27402037+GerrickB@users.noreply.github.com>
2022-12-13 15:05:52 -0800

    Write pseudocode for cleaning phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -55,6 +55,16 @@ contents.each do |row|
 
   zipcode = clean_zip_code(row[:zipcode])
 
+  #If the phone number is less than 10 digits, assume that it is a bad number
+
+  #If the phone number is 10 digits, assume that it is good
+
+  #If the phone number is 11 digits and the first number is 1, trim the 1 and use the remaining 10 digits
+
+  #If the phone number is 11 digits and the first number is not 1, then it is a bad number
+
+  #If the phone number is more than 11 digits, assume that it is a bad number
+
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
